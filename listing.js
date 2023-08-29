3a8dcca299a6e4e298e98885313d3338522f0cd7
Aistis Vyšniauskas <devpastas@gmail.com>
2023-08-29 18:24:19 +0300

    added delete button and add product page

--- listing.js
+++ listing.js
@@ -1,10 +1,30 @@
 console.log(localStorage.getItem("listingId"));
-
+const listingContainer = document.getElementById("listingcontainer");
 
 const getSelectedListing = async function(){
-    const selectedListing = await fetch("https://64ec4b1af9b2b70f2bfa0da0.mockapi.io/listings/" + localStorage.getItem("listingId"));
-    const parsedListing = await selectedListing.json();
-    alert(parsedListing.price);
+    const response = await fetch("https://64ec4b1af9b2b70f2bfa0da0.mockapi.io/listings/" + localStorage.getItem("listingId"));
+    const listingData = await response.json();
+    
+    const image = document.createElement("img");
+    image.src = listingData.img_url;
+    image.alt = "Image of listed product";
+    listingContainer.append(image);
+    
+    const title = document.createElement("h1");
+    title.textContent = listingData.title;
+    listingContainer.append(title);
+
+    const description = document.createElement("p");
+    description.textContent = listingData.description;
+    listingContainer.append(description);
+
+    const location = document.createElement()
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "DELETE";
+    
+    listingContainer.append(deleteBtn);
+    console.log(listingData);
 }
 
 getSelectedListing(); 
